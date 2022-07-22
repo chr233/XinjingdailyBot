c58ca32a2826c93bab377f64f52892f29f80a66b
Chr_ <chr@chrxw.com>
2022-07-22 17:18:21 +0800

    还没完全写完，存个档先

--- XinjingdailyBot/Helpers/ChannelHelper.cs
+++ XinjingdailyBot/Helpers/ChannelHelper.cs
@@ -18,10 +18,48 @@ namespace XinjingdailyBot.Helpers
         /// 闲聊区群组
         /// </summary>
         internal static Chat SubGroup { get; private set; } = new();
+        /// <summary>
+        /// 发布频道
+        /// </summary>
         internal static Chat AcceptChannel { get; private set; } = new();
+        /// <summary>
+        /// 拒稿存档
+        /// </summary>
         internal static Chat RejectChannel { get; private set; } = new();
+        /// <summary>
+        /// 机器人用户信息
+        /// </summary>
+        internal static User BotUser { get; private set; } = new();
         internal static async Task VerifyChannelConfig(ITelegramBotClient botClient)
         {
+            BotUser = await botClient.GetMeAsync();
+
+            Logger.Info($"机器人ID: {BotUser.Id}");
+            Logger.Info($"机器人昵称: {BotUser.NickName()}");
+            Logger.Info($"机器人用户名: @{BotUser.Username}");
+
+            try
+            {
+                AcceptChannel = await botClient.GetChatAsync(BotConfig.AcceptChannel);
+                Logger.Info($"稿件发布频道: {AcceptChannel.ChatProfile()}");
+            }
+            catch
+            {
+                Logger.Error("未找到指定的稿件发布频道, 请检查拼写是否正确");
+                throw;
+            }
+
+            try { 
+
+                RejectChannel = await botClient.GetChatAsync(BotConfig.RejectChannel);
+                Logger.Info($"拒稿存档频道: {RejectChannel.ChatProfile()}");
+            }
+            catch 
+            {
+                Logger.Error("未找到指定的拒稿存档频道, 请检查拼写是否正确");
+                throw;
+            }
+
             try
             {
                 if (long.TryParse(BotConfig.ReviewGroup, out long groupId))
@@ -32,6 +70,7 @@ namespace XinjingdailyBot.Helpers
                 {
                     ReviewGroup = await botClient.GetChatAsync(BotConfig.ReviewGroup);
                 }
+                Logger.Info($"审核群组: {ReviewGroup.ChatProfile()}");
             }
             catch
             {
@@ -49,6 +88,7 @@ namespace XinjingdailyBot.Helpers
                 {
                     CommentGroup = await botClient.GetChatAsync(BotConfig.CommentGroup);
                 }
+                Logger.Info($"评论区群组: {CommentGroup.ChatProfile()}");
             }
             catch
             {
@@ -66,18 +106,13 @@ namespace XinjingdailyBot.Helpers
                 {
                     SubGroup = await botClient.GetChatAsync(BotConfig.SubGroup);
                 }
+                Logger.Info($"频道子群组: {SubGroup.ChatProfile()}");
             }
             catch
             {
                 Logger.Error("未找到指定的闲聊群组, 可以使用 /groupinfo 命令获取群组信息");
                 SubGroup = new() { Id = -1 };
             }
-
-            AcceptChannel = await botClient.GetChatAsync(BotConfig.AcceptChannel);
-            RejectChannel = await botClient.GetChatAsync(BotConfig.RejectChannel);
-
-            User me = await botClient.GetMeAsync();
-            BotName = me.Username ?? "";
         }
     }
 }
