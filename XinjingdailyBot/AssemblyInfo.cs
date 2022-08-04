ac5b1d0ca27f653eae7f67fe2a15680967750f04
Chr_ <chr@chrxw.com>
2022-08-04 12:41:07 +0800

    更新版本号

--- XinjingdailyBot/AssemblyInfo.cs
+++ XinjingdailyBot/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("1.0.4.531")]
-[assembly: AssemblyFileVersion("1.0.4.531")]
+[assembly: AssemblyVersion("1.0.4.532")]
+[assembly: AssemblyFileVersion("1.0.4.532")]
 
 [assembly: AssemblyCopyright("Copyright @ 2022 Chr_")]
 [assembly: AssemblyProduct("XinjingDaily Bot")]
