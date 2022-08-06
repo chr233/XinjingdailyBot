c86e63cc1f18d08950974d9276564737ccd18a6b
Chr_ <chr@chrxw.com>
2022-08-06 22:58:00 +0800

    feat 新增定时任务模块, 用于处理超时未投递的稿件以及超时未审核的稿件, 后者自动计入ExpiredPostCount

---
+++ XinjingdailyBot/Tasks/TaskTrigger.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+using static XinjingdailyBot.Utils;
+
+namespace XinjingdailyBot.Tasks
+{
+    internal static class TaskTrigger
+    {
+        //TODO
+
+        //private static Dictionary<string, Task> TaskList = new();
+        //private static Dictionary<string, TimeSpan> TaskInterval = new();
+        //private static Dictionary<string, DateTime> TaskNextExec = new();
+
+        private static TimeSpan TaskPeriod { get; }
+
+        private static DateTime LastExec { get; set; }
+
+        static TaskTrigger()
+        {
+            TaskPeriod = TimeSpan.FromDays(3);
+            LastExec = DateTime.MinValue;
+        }
+
+        internal static async Task HandleTick(ITelegramBotClient botClient)
+        {
+            DateTime now = DateTime.Now;
+            if (now - LastExec >= TaskPeriod)
+            {
+                LastExec = now;
+                await ExpiredPostsTask.MarkExpiredPost(botClient);
+            }
+        }
+    }
+}
