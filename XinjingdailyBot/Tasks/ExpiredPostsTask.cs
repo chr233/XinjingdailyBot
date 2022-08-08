652c60c295ec81e85043a5259906565c5f26c07b
Chr_ <chr@chrxw.com>
2022-08-08 12:16:46 +0800

    refactor 重构定时处理过期投稿，尝试解决上一个版本的问题

--- XinjingdailyBot/Tasks/ExpiredPostsTask.cs
+++ XinjingdailyBot/Tasks/ExpiredPostsTask.cs
@@ -9,8 +9,10 @@ namespace XinjingdailyBot.Tasks
 {
     internal static class ExpiredPostsTask
     {
+        /// <summary>
+        /// 超时时间设定
+        /// </summary>
         private static TimeSpan PostTimeout { get; } = TimeSpan.FromDays(3);
-        private static int PostCount { get; } = 50;
 
         internal static async Task MarkExpiredPost(ITelegramBotClient botClient)
         {
@@ -18,127 +20,98 @@ namespace XinjingdailyBot.Tasks
 
             DateTime expiredDate = DateTime.Now - PostTimeout;
 
-            HashSet<long> userIDs = new();
+            //获取有过期稿件的用户
+            var userIDList = await DB.Queryable<Posts>()
+                .Where(x => (x.Status == PostStatus.Padding || x.Status == PostStatus.Reviewing) && x.ModifyAt < expiredDate)
+                .Distinct().Select(x => x.PosterUID).ToListAsync();
 
-            //标记过期投稿
-            Dictionary<long, int> expiredPostDict = new();
-            var paddingPosts = await DB.Queryable<Posts>().Where(x => x.Status == PostStatus.Padding && x.ModifyAt < expiredDate).ToPageListAsync(1, PostCount);
-
-            if (paddingPosts.Any())
+            if (!userIDList.Any())
             {
+                Logger.Info("T 结束定时任务, 没有需要清理的过期稿件");
+                return;
+            }
+
+            Logger.Info($"T 成功获取 {userIDList.Count} 个有过期稿件的用户");
+
+            foreach (var userID in userIDList)
+            {
+                //获取过期投稿
+                var paddingPosts = await DB.Queryable<Posts>()
+                    .Where(x => x.PosterUID == userID && (x.Status == PostStatus.Padding || x.Status == PostStatus.Reviewing) && x.ModifyAt < expiredDate)
+                    .ToListAsync();
+
+                if (!paddingPosts.Any())
+                {
+                    continue;
+                }
+
+                int cTmout = 0, rTmout = 0;
                 foreach (var post in paddingPosts)
                 {
-                    post.Status = PostStatus.ConfirmTimeout;
-                    post.ModifyAt = DateTime.Now;
-
-                    if (!expiredPostDict.TryAdd(post.PosterUID, 1))
+                    if (post.Status == PostStatus.Padding)
                     {
-                        expiredPostDict[post.PosterUID]++;
+                        post.Status = PostStatus.ConfirmTimeout;
+                        cTmout++;
                     }
                     else
                     {
-                        userIDs.Add(post.PosterUID);
+                        post.Status = PostStatus.ReviewTimeout;
+                        rTmout++;
                     }
+                    post.ModifyAt = DateTime.Now;
                 }
+
                 await DB.Updateable(paddingPosts).UpdateColumns(x => new { x.Status, x.ModifyAt }).ExecuteCommandAsync();
 
-                Logger.Info($"T 清理了 {paddingPosts.Count} 条确认超时的投稿");
-            }
+                var user = await DB.Queryable<Users>().FirstAsync(x => x.UserID == userID);
 
-            //标记审核超时的投稿
-            Dictionary<long, int> timeoutPostDict = new();
-            var timeoutPosts = await DB.Queryable<Posts>().Where(x => x.Status == PostStatus.Reviewing && x.ModifyAt < expiredDate).ToPageListAsync(1, PostCount);
-
-            Logger.Debug($"T 获取 {timeoutPosts.Count} 条审核超时投稿");
-
-            if (timeoutPosts.Any())
-            {
-                foreach (var post in timeoutPosts)
+                if (user == null)
                 {
-                    post.Status = PostStatus.ReviewTimeout;
-                    post.ModifyAt = DateTime.Now;
-
-                    if (!timeoutPostDict.TryAdd(post.PosterUID, 1))
-                    {
-                        timeoutPostDict[post.PosterUID]++;
-                    }
-                    else
-                    {
-                        userIDs.Add(post.PosterUID);
-                    }
-                }
-                await DB.Updateable(timeoutPosts).UpdateColumns(x => new { x.Status, x.ModifyAt }).ExecuteCommandAsync();
-
-                Logger.Info($"T 清理了 {timeoutPosts.Count} 条审核超时的投稿");
-            }
-
-            //获取用户数据
-            var userList = await DB.Queryable<Users>().Where(x => userIDs.Contains(x.UserID)).ToListAsync();
-
-            Logger.Debug($"T 获取 {userList.Count} 条相关用户");
-
-            List<Users> userUpdateList = new();
-
-            //通知投稿人
-            StringBuilder sb = new();
-            foreach (var user in userList)
-            {
-                sb.Clear();
-                if (expiredPostDict.TryGetValue(user.UserID, out int confirmTimeoutCount))
-                {
-                    sb.AppendLine($"你有 <code>{confirmTimeoutCount}</code> 份稿件确认超时");
+                    Logger.Info($"T 清理了 {userID} 的 {cTmout} / {rTmout} 条确认/审核超时投稿");
                 }
                 else
                 {
-                    confirmTimeoutCount = 0;
-                }
+                    Logger.Info($"T 清理了 {user} 的 {cTmout} / {rTmout} 条确认/审核超时投稿");
 
-                if (timeoutPostDict.TryGetValue(user.UserID, out int revirwTimeoutCount))
-                {
-                    sb.AppendLine($"你有 <code>{revirwTimeoutCount}</code> 份稿件审核超时");
-                }
-                else
-                {
-                    revirwTimeoutCount = 0;
-                }
-
-                //满足条件则发送通知
-                //1.未封禁
-                //2.有PrivateChatID
-                //3.启用通知
-                //4.消息不为空
-                if (!user.IsBan && user.PrivateChatID > 0 && user.Notification && sb.Length > 0)
-                {
-                    sb.AppendLine();
-                    sb.AppendLine("可以使用命令 /notificate 开启或关闭此提示");
-                    try
+                    //满足条件则通知投稿人
+                    //1.未封禁
+                    //2.有PrivateChatID
+                    //3.启用通知
+                    if (!user.IsBan && user.PrivateChatID > 0 && user.Notification)
                     {
-                        await botClient.SendTextMessageAsync(user.PrivateChatID, sb.ToString(), ParseMode.Html, disableNotification: true);
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.Error($"T 通知消息发送失败, 自动禁用更新 {ex.GetType()} {ex.Message}");
+                        StringBuilder sb = new();
 
-                        user.PrivateChatID = -1;
+                        if (cTmout > 0)
+                        {
+                            sb.AppendLine($"你有 <code>{cTmout}</code> 份稿件确认超时");
+                        }
 
-                        await Task.Delay(400);
+                        if (rTmout > 0)
+                        {
+                            sb.AppendLine($"你有 <code>{rTmout}</code> 份稿件审核超时");
+                        }
+                        sb.AppendLine();
+                        sb.AppendLine("可以使用命令 /notification 开启或关闭此提示");
+
+                        try
+                        {
+                            await botClient.SendTextMessageAsync(user.PrivateChatID, sb.ToString(), ParseMode.Html, disableNotification: true);
+                            await Task.Delay(100);
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Error($"T 通知消息发送失败, 自动禁用更新 {ex.GetType()} {ex.Message}");
+                            user.PrivateChatID = -1;
+                            await Task.Delay(500);
+                        }
                     }
-                    finally
-                    {
-                        await Task.Delay(100);
-                    }
+
+                    user.ExpiredPostCount += rTmout;
+                    user.ModifyAt = DateTime.Now;
+
+                    //更新用户表
+                    await DB.Updateable(user).UpdateColumns(x => new { x.PrivateChatID, x.ExpiredPostCount, x.ModifyAt }).ExecuteCommandAsync();
                 }
-
-                user.ExpiredPostCount += revirwTimeoutCount;
-                user.ModifyAt = DateTime.Now;
-                userUpdateList.Add(user);
-            }
-
-            //更新用户表
-            if (userUpdateList.Any())
-            {
-                await DB.Updateable(userUpdateList).UpdateColumns(x => new { x.PrivateChatID, x.ModifyAt }).ExecuteCommandAsync();
-                Logger.Info($"T 更新了 {userUpdateList.Count} 个用户");
             }
         }
     }
