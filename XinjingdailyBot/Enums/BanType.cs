fe2b0caeb60139b1fc665972f706f670d5b2fdc1
Chr_ <chr@chrxw.com>
2022-08-10 17:31:08 +0800

    feat 数据库结构修改, ban 表 IsBan -> Type, 数据类型改为 int(11)

---
+++ XinjingdailyBot/Enums/BanType.cs
@@ -0,0 +1,18 @@
+﻿namespace XinjingdailyBot.Enums
+{
+    internal enum BanType : byte
+    {
+        /// <summary>
+        /// 解封
+        /// </summary>
+        UnBan = 0,
+        /// <summary>
+        /// 封禁
+        /// </summary>
+        Ban = 1,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        Warning = 2,
+    }
+}
