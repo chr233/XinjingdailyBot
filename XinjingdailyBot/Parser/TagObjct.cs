d7964b71684cad8eb8782568adb7e5ca1216cfa8
Chr_ <chr@chrxw.com>
2022-08-10 17:20:37 +0800

    feat 投稿内容可以保留格式了

---
+++ XinjingdailyBot/Parser/TagObjct.cs
@@ -0,0 +1,32 @@
+﻿namespace XinjingdailyBot.Parser
+{
+    internal sealed class TagObjct
+    {
+        private List<string> Content { get; }
+
+        public TagObjct()
+        {
+            Content = new();
+        }
+        public TagObjct(string content) : this()
+        {
+            Content.Add(content);
+        }
+
+        public void AddLast(string value)
+        {
+            Content.Add(value);
+        }
+
+        public void AddFirst(string value)
+        {
+            Content.Insert(0, value);
+        }
+
+
+        public override string? ToString()
+        {
+            return string.Join("", Content);
+        }
+    }
+}
