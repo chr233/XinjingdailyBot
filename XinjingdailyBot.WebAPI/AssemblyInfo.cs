b6a62daec4c3f9bb4ccca920a259919f932ccd0a
Chr_ <chr@chrxw.com>
2023-01-16 14:05:57 +0800

    bugfix

--- XinjingdailyBot.WebAPI/AssemblyInfo.cs
+++ XinjingdailyBot.WebAPI/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("2.0.8.1")]
+[assembly: AssemblyVersion("2.0.8.2")]
 
 [assembly: AssemblyCopyright("Copyright @ 2023 Chr_")]
 [assembly: AssemblyProduct("XinjingDaily Bot")]
