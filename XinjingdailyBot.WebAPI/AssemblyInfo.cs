78ab1590acb1273eacb919b7b3da9e5db0bdba2c
chr_ <chr@chrxw.com>
2023-02-03 22:15:19 +0800

    misc 更改版本号

--- XinjingdailyBot.WebAPI/AssemblyInfo.cs
+++ XinjingdailyBot.WebAPI/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("2.0.12.1")]
+[assembly: AssemblyVersion("2.0.13.0")]
 
 [assembly: AssemblyCopyright("Copyright @ 2023 Chr_")]
 [assembly: AssemblyProduct("XinjingDaily Bot")]
