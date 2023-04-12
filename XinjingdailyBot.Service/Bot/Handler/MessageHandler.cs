cf71582110602ed9f4c9c939cd9d1cd6fe377870
chr_ <chr@chrxw.com>
2023-04-12 17:27:53 +0800

    update

--- XinjingdailyBot.Service/Bot/Handler/MessageHandler.cs
+++ XinjingdailyBot.Service/Bot/Handler/MessageHandler.cs
@@ -47,13 +47,14 @@ namespace XinjingdailyBot.Service.Bot.Handler
                 if (message.ForwardFromChat != null)
                 {
                     var handled = await _forwardMessageHandler.OnForwardMessageReceived(dbUser, message);
-                    if (!handled)
+                    if (handled)
                     {
-                        if (await _postService.CheckPostLimit(dbUser, message, null))
-                        {
-                            await _postService.HandleTextPosts(dbUser, message);
-                        }
+                        return;
                     }
+                }
+                if (await _postService.CheckPostLimit(dbUser, message, null))
+                {
+                    await _postService.HandleTextPosts(dbUser, message);
                 }
             }
             else if (message.Chat.Type == ChatType.Group || message.Chat.Type == ChatType.Supergroup)
@@ -82,32 +83,33 @@ namespace XinjingdailyBot.Service.Bot.Handler
                     var handled = await _forwardMessageHandler.OnForwardMessageReceived(dbUser, message);
                     if (!handled)
                     {
-                        switch (message.Type)
-                        {
-                            case MessageType.Photo:
-                            case MessageType.Audio:
-                            case MessageType.Video:
-                            case MessageType.Voice:
-                            case MessageType.Document:
-                            case MessageType.Animation:
-                                if (await _postService.CheckPostLimit(dbUser, message, null))
-                                {
-                                    if (message.MediaGroupId != null)
-                                    {
-                                        await _postService.HandleMediaGroupPosts(dbUser, message);
-                                    }
-                                    else
-                                    {
-                                        await _postService.HandleMediaPosts(dbUser, message);
-                                    }
-                                }
-
-                                break;
-                            default:
-                                await _botClient.AutoReplyAsync($"暂不支持的投稿类型 {message.Type}", message);
-                                break;
-                        }
+                        return;
                     }
+                }
+                switch (message.Type)
+                {
+                    case MessageType.Photo:
+                    case MessageType.Audio:
+                    case MessageType.Video:
+                    case MessageType.Voice:
+                    case MessageType.Document:
+                    case MessageType.Animation:
+                        if (await _postService.CheckPostLimit(dbUser, message, null))
+                        {
+                            if (message.MediaGroupId != null)
+                            {
+                                await _postService.HandleMediaGroupPosts(dbUser, message);
+                            }
+                            else
+                            {
+                                await _postService.HandleMediaPosts(dbUser, message);
+                            }
+                        }
+
+                        break;
+                    default:
+                        await _botClient.AutoReplyAsync($"暂不支持的投稿类型 {message.Type}", message);
+                        break;
                 }
             }
         }
