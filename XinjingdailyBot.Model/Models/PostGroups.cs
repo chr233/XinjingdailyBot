014143d8412bf0103d2d2ca03a60be935ef9f487
chr_ <chr@chrxw.com>
2023-04-13 18:17:12 +0800

    update

---
+++ XinjingdailyBot.Model/Models/PostGroups.cs
@@ -0,0 +1,26 @@
+﻿using SqlSugar;
+using XinjingdailyBot.Model.Base;
+
+namespace XinjingdailyBot.Model.Models
+{
+    [SugarTable("post_group", TableDescription = "媒体组稿件记录")]
+    [SugarIndex("index_postid", nameof(PostID), OrderByType.Asc)]
+    [SugarIndex("index_reviewerid", nameof(PublicMsgID), OrderByType.Asc)]
+    public sealed record PostGroups : BaseModel
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        /// <summary>
+        /// 稿件ID
+        /// </summary>
+        public int PostID { get; set; } = -1;
+        /// <summary>
+        /// 发布的消息Id
+        /// </summary>
+        public long PublicMsgID { get; set; } = -1;
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+}
