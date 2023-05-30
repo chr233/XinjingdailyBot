1584a406c472aa30473043c9ddef8823e2fc665c
Chr_ <chr@chrxw.com>
2023-05-30 23:00:00 +0800

    bugfix feat 用户Token相关

--- XinjingdailyBot.WebAPI/Controllers/IndexController.cs
+++ XinjingdailyBot.WebAPI/Controllers/IndexController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using XinjingdailyBot.Interface.Data;
+using XinjingdailyBot.Model.Models;
 
 namespace XinjingdailyBot.WebAPI.Controllers
 {
@@ -10,14 +12,19 @@ namespace XinjingdailyBot.WebAPI.Controllers
     public class IndexController : ControllerBase
     {
         private readonly ILogger<IndexController> _logger;
+        private readonly IUserTokenService _userTokenService;
 
         /// <summary>
         /// 构造函数
         /// </summary>
         /// <param name="logger"></param>
-        public IndexController(ILogger<IndexController> logger)
+        /// <param name="userTokenService"></param>
+        public IndexController(
+            ILogger<IndexController> logger,
+            IUserTokenService userTokenService)
         {
             _logger = logger;
+            _userTokenService = userTokenService;
         }
 
         /// <summary>
@@ -28,6 +35,26 @@ namespace XinjingdailyBot.WebAPI.Controllers
         public IActionResult Get()
         {
             return Ok("启动完成");
+        }
+
+        /// <summary>
+        /// 测试
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<Users>> TestToken([FromBody] Guid token)
+        {
+            var user = await _userTokenService.VerifyToken(token);
+
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
