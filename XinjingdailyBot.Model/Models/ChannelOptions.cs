c2f03d76f4170fad4782c8369a8997ad6ab40844
Chr_ <chr@chrxw.com>
2024-01-29 20:11:19 +0800

    删除多余索引

--- XinjingdailyBot.Model/Models/ChannelOptions.cs
+++ XinjingdailyBot.Model/Models/ChannelOptions.cs
@@ -11,7 +11,6 @@ namespace XinjingdailyBot.Model.Models;
 [SugarTable("channel", TableDescription = "来源频道设定")]
 [SugarIndex("index_channel_id", nameof(ChannelID), OrderByType.Asc, true)]
 [SugarIndex("index_channel_name", nameof(ChannelName), OrderByType.Asc, false)]
-[SugarIndex("index_channel_title", nameof(ChannelTitle), OrderByType.Asc, false)]
 public sealed record ChannelOptions : BaseModel, ICreateAt, IModifyAt
 {
     /// <summary>
