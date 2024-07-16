882866665efcab3c5498c9bdccf92dcbdf145151
chr_ <chr@chrxw.com>
2024-07-16 11:30:10 +0800

    misc

--- XinjingdailyBot.Infrastructure/BuildInfo.cs
+++ XinjingdailyBot.Infrastructure/BuildInfo.cs
@@ -7,34 +7,34 @@ namespace XinjingdailyBot.Infrastructure;
 /// </summary>
 public static class BuildInfo
 {
-#if XJB_VARIANT_GENERIC
-		    internal static bool CanUpdate => false;
-		    internal static string Variant => "generic";
-#elif XJB_VARIANT_LINUX_ARM
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "linux-arm";
-#elif XJB_VARIANT_LINUX_ARM64
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "linux-arm64";
-#elif XJB_VARIANT_LINUX_X64
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "linux-x64";
-#elif XJB_VARIANT_OSX_ARM64
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "osx-arm64";
-#elif XJB_VARIANT_OSX_X64
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "osx-x64";
-#elif XJB_VARIANT_WIN_ARM64
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "win-arm64";
-#elif XJB_VARIANT_WIN_X64
-		    internal static bool CanUpdate => true;
-		    internal static string Variant => "win-x64";
+#if XJB_GENERIC
+	publish const bool CanUpdate = false;
+	publish const string Variant = "generic";
+#elif XJB_LINUX_ARM
+	publish const bool CanUpdate = true;
+	publish const string Variant = "linux-arm";
+#elif XJB_LINUX_ARM64
+	publish const bool CanUpdate = true;
+	publish const string Variant = "linux-arm64";
+#elif XJB_LINUX_X64
+	publish const bool CanUpdate = true;
+	publish const string Variant = "linux-x64";
+#elif XJB_OSX_ARM64
+	publish const bool CanUpdate = true;
+	publish const string Variant = "osx-arm64";
+#elif XJB_OSX_X64
+	publish const bool CanUpdate = true;
+	publish const string Variant = "osx-x64";
+#elif XJB_WIN_ARM64
+	publish const bool CanUpdate = true;
+	public const string Variant = "win-arm64";
+#elif XJB_WIN_X64
+	public const bool CanUpdate = true;
+	public const string Variant = "win-x64";
 #else
-    public static bool CanUpdate => false;
-    public static string Variant => "source";
+    public const bool CanUpdate = false;
+    public const string Variant = "source";
 #endif
-    public static string Author => "chr233";
-    public static string Repo => "https://github.com/chr233/XinjingdailyBot/";
+    public const string Author = "chr233";
+    public const string Repo = "https://github.com/chr233/XinjingdailyBot/";
 }
--- XinjingdailyBot.WebAPI/Program.cs
+++ XinjingdailyBot.WebAPI/Program.cs
@@ -3,8 +3,6 @@ using XinjingdailyBot.Infrastructure;
 using XinjingdailyBot.Infrastructure.Localization;
 using XinjingdailyBot.WebAPI.Extensions;
 
-var _logger = NLog.LogManager.GetCurrentClassLogger();
-
 const string banner = @"
 __  _ _             _  _            ___       _  _      
 \ \/ <_>._ _  ___  <_><_>._ _  ___ | . \ ___ <_>| | _ _ 
@@ -13,21 +11,21 @@ _/\_\|_||_|_|\_. | | ||_||_|_|\_. ||___/<___||_||_|`_. |
              <___'<__'        <___'                <___'           
 ";
 
-_logger.Info(Langs.Line);
+Console.WriteLine(Langs.Line);
 foreach (var line in banner.Split('\n'))
 {
-    _logger.Info(line);
+    Console.WriteLine(line);
 }
-_logger.Info(Langs.Line);
-_logger.Info("框架: {0}", Utils.FrameworkName);
-_logger.Info("版本: {0} {1} {2}", Utils.Version, Utils.Configuration, BuildInfo.Variant);
-_logger.Info("作者: {0} {1}", BuildInfo.Author, Utils.Company);
-_logger.Info("版权: {0}", Utils.Copyright);
-_logger.Info(Langs.Line);
-_logger.Info("欢迎使用 XinjingdailyBot");
-_logger.Info(Langs.Line);
-_logger.Warn("欢迎订阅心惊报 https://t.me/xinjingdaily");
-_logger.Info(Langs.Line);
+Console.WriteLine(Langs.Line);
+Console.WriteLine("框架: {0}", Utils.FrameworkName);
+Console.WriteLine("版本: {0} {1} {2}", Utils.Version, Utils.Configuration, BuildInfo.Variant);
+Console.WriteLine("作者: {0} {1}", BuildInfo.Author, Utils.Company);
+Console.WriteLine("版权: {0}", Utils.Copyright);
+Console.WriteLine(Langs.Line);
+Console.WriteLine("欢迎使用 XinjingdailyBot");
+Console.WriteLine(Langs.Line);
+Console.WriteLine("欢迎订阅心惊报 https://t.me/xinjingdaily");
+Console.WriteLine(Langs.Line);
 
 Thread.Sleep(2000);
 
